0c41395920a2c37e5d123b8627479d631c3f42b9
Tayseer96 <mohamad.alshahrour@student.ehb.be>
2022-01-24 10:02:48 +0100

    DEBUG: script update path

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 "use strict";
-import context from "../../scripts/context.js";
-import * as Utils from "../../scripts/utils.js";
+import context from "./../scripts/context.js";
+import * as Utils from "./../scripts/utils.js";
 
 let width = context.canvas.width;
 let height = context.canvas.height;
