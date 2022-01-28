f4dc070ad6dcbdf27a640a70f71255a1fa9382c8
Tayseer96 <mohamad.alshahrour@student.ehb.be>
2022-01-28 13:29:49 +0100

    vrijdag

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
-// $(window).scroll(function () {
-//     var scrollAfstand = $(window).scrollTop();
-//     var hoogteBlock1 = $("#home").outerHeight();
+ $(window).scroll(function () {
+     //     var scrollAfstand = $(window).scrollTop();
+     //     var hoogteBlock1 = $("#home").outerHeight();
 
-//     if (scrollAfstand >= 300) {
-//         //true
-//         $("#main-menu").addClass("fixedNav");
-//         $("#main-menu").stop();
-//         $("#main-menu").animate({
-//             width: '80%',
-//         }, 1000);
+     //     if (scrollAfstand >= 300) {
+     //         //true
+     //         $("#main-menu").addClass("fixedNav");
+     //         $("#main-menu").stop();
+     //         $("#main-menu").animate({
+     //             width: '80%',
+     //         }, 1000);
 
-//     } else {
-//         //false
-//         $("#main-menu").removeClass("fixedNav");
-//         $("#main-menu").stop();
-//         $("#main-menu").animate({
-//             width: '100%'
-//         }, 1000);
-//     }
-// });
-
-// function changeVideo() {
-//     document.getElementById("example_video_1").src = "videos/fullProject-Video-Contact-Active.mp4/";
-// }
+     //     } else {
+     //         //false
+     //         $("#main-menu").removeClass("fixedNav");
+     //         $("#main-menu").stop();
+     //         $("#main-menu").animate({
+     //             width: '100%'
+     //         }, 1000);
+     //     }
+     //  $("#button").click(function () {
+     //      alert('test');
+     //  });
+ });
