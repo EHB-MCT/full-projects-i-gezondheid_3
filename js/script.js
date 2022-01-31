750faf962884e830c13b544970bb738cdc3c1890
Tayseer96 <mohamad.alshahrour@student.ehb.be>
2022-01-31 15:45:29 +0100

    maandag

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
- $(window).scroll(function () {
-     //     var scrollAfstand = $(window).scrollTop();
-     //     var hoogteBlock1 = $("#home").outerHeight();
+// var hasAdjusted = false;
 
-     //     if (scrollAfstand >= 300) {
-     //         //true
-     //         $("#main-menu").addClass("fixedNav");
-     //         $("#main-menu").stop();
-     //         $("#main-menu").animate({
-     //             width: '80%',
-     //         }, 1000);
+// $(window).scroll(function () {
+//     if ($(document).width() > 400) {
 
-     //     } else {
-     //         //false
-     //         $("#main-menu").removeClass("fixedNav");
-     //         $("#main-menu").stop();
-     //         $("#main-menu").animate({
-     //             width: '100%'
-     //         }, 1000);
-     //     }
-     //  $("#button").click(function () {
-     //      alert('test');
-     //  });
- });
+//         var scrollAfstand = $(window).scrollTop();
+//         //  var hoogteBlock1 = $("#home").outerHeight();
+//         console.log(scrollAfstand)
+//         if (scrollAfstand >= 300 && !hasAdjusted) {
+//             //true
+//             $("#main-menu").addClass("fixedNav");
+//             $("#main-menu").stop();
+//             $("#main-menu").animate({
+//                 width: '80%',
+//             }, 1000);
+//             hasAdjusted = true;
+
+//         } else if (hasAdjusted) {
+//             // //false
+//             // $("#main-menu").removeClass("fixedNav");
+//             // $("#main-menu").stop();
+//             // $("#main-menu").animate({
+//             //     width: '100%'
+//             // }, 1000);
+//             hasAdjusted = false;
+//         }
+//     }
+// });
+// var myVideo = document.getElementById("myVideo");
+
+
+function myFunction() {
+    // myVideo.src = "videos/fullProject-Video-Contact-Active.mp4";
+    document.getElementById("video-1").style.display = "none";
+    document.getElementById("video-2").style.display = "inline";
+}
+
+
+document.getElementById('myVideo2').addEventListener('ended', function () {
+    document.getElementById("video-1").style.display = "inline";
+    document.getElementById("video-2").style.display = "none";
+    location.reload();
+});
