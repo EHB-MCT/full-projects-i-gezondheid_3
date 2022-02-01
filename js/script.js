c222812c1728c67738ebbc6a8e0568a28fa7d248
Tayseer96 <mohamad.alshahrour@student.ehb.be>
2022-02-01 17:15:49 +0100

    dinsdag

--- js/script.js
+++ js/script.js
@@ -1,34 +1,3 @@
-// var hasAdjusted = false;
-
-// $(window).scroll(function () {
-//     if ($(document).width() > 400) {
-
-//         var scrollAfstand = $(window).scrollTop();
-//         //  var hoogteBlock1 = $("#home").outerHeight();
-//         console.log(scrollAfstand)
-//         if (scrollAfstand >= 300 && !hasAdjusted) {
-//             //true
-//             $("#main-menu").addClass("fixedNav");
-//             $("#main-menu").stop();
-//             $("#main-menu").animate({
-//                 width: '80%',
-//             }, 1000);
-//             hasAdjusted = true;
-
-//         } else if (hasAdjusted) {
-//             // //false
-//             // $("#main-menu").removeClass("fixedNav");
-//             // $("#main-menu").stop();
-//             // $("#main-menu").animate({
-//             //     width: '100%'
-//             // }, 1000);
-//             hasAdjusted = false;
-//         }
-//     }
-// });
-// var myVideo = document.getElementById("myVideo");
-
-
 function myFunction() {
     // myVideo.src = "videos/fullProject-Video-Contact-Active.mp4";
     document.getElementById("video-1").style.display = "none";
